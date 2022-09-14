78cf84cce3215231c331077725b451489b6b481c
BEATRIZ <beatrizzquaresma@gmail.com>
2022-09-14 19:00:53 -0400

    atualização na função jogo

--- jogo da velha/jogo.py
+++ jogo da velha/jogo.py
@@ -1,5 +1,5 @@
 matriz = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-def mapaDasPosicoes():
+def mapaDasPosições():
     print("~" * 30)
     print(f'{"JOGO DA VELHA":^30}')
     print("~" * 30)
@@ -11,46 +11,35 @@ def mapaDasPosicoes():
     print("~" * 30)
 
 def tabuleiro(matriz): #cria o tabuleiro vazio
-    
   for c in range(0, 3):
     for l in range(0, 3):
       print(f'[{matriz[c][l]}]', end='')
     print()
 
-def preenchimento(tabuleiro, jogada, simb): #preenche as casas com as jogadas
-    if jogada == 1:
-        matriz[0][0] = simb
-    if jogada == 2:
-        matriz[0][1] = simb
-    if jogada == 3:
-        matriz[0][2] = simb
-    if jogada == 4:
-        matriz[1][0] = simb
-    if jogada == 5:
-        matriz[1][1] = simb
-    if jogada == 6:
-        matriz[1][2] = simb
-    if jogada == 7:
-        matriz[2][0] = simb
-    if jogada == 8:
-        matriz[2][1] = simb
-    if jogada == 9:
-        matriz[2][2] = simb
+def jogo(tabuleiro, jogada, simb): #preenche o tabuleiro e verifica se a casa já está preenchida ou se foi digitado número errado
+  posicoes = {
+    1: (0, 0),
+    2: (0, 1),
+    3: (0, 2),
+    4: (1, 0),
+    5: (1, 1),
+    6: (1, 2),
+    7: (2, 0),
+    8: (2, 1),
+    9: (2, 2)
+  }
+  
+  if jogada not in posicoes:
+    return False
 
-def tenteDeNovo(tabuleiro, jogada, simb): #caso a casa esteja preenchida ou o jogador digite um número errado
-    posicoes = {1: matriz[0][0],
-                2: matriz[0][1],
-                3: matriz[0][2],
-                4: matriz[1][0],
-                5: matriz[1][1],
-                6: matriz[1][2],
-                7: matriz[2][0],
-                8: matriz[2][1],
-                9: matriz[2][2]
-    }
+  c, l = posicoes[jogada]
 
-    if jogada < 0 or jogada > 9:
-        return True
+  if matriz[c][l] in 'xo':
+    return False
+
+  matriz[c][l] = simb
+
+  return True
        
 def temGanhador(tabuleiro, simb): #verifica se tem ganhador
     #comparaçõpes na horizontal
@@ -75,7 +64,7 @@ def temGanhador(tabuleiro, simb): #verifica se tem ganhador
 
 def main(): #jogo
     
-    mapaDasPosicoes()
+    mapaDasPosições()
     cont = 0
     while True:
         jogada = int(input('Sua jogada: '))
@@ -83,12 +72,12 @@ def main(): #jogo
             simb = 'x'
         else:
             simb = 'o'
-        preenchimento(tabuleiro, jogada, simb)
-        tabuleiro(matriz)
-        if tenteDeNovo(tabuleiro, jogada, simb):
+        
+        if not jogo(tabuleiro, jogada, simb):
             print("Jogada Inválida! Tente Novamente!")
             continue
         
+        tabuleiro(matriz)
         if temGanhador(tabuleiro, simb):
             print(f'Jogador "{simb}" é o VENCEDOR!')
             break
@@ -99,3 +88,4 @@ def main(): #jogo
             break
 
 main()
+
